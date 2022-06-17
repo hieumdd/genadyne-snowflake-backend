2ad9e581eec5c078cb255c4ce97594234bcd1d3f
HM <hieumdd@gmail.com>
2022-06-18 00:19:29 +0700

    fix: Fix route slug

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,3 @@
-const routes: Record<string, string> = {
-    '/patient': 'SELECT * from "LIVE DATA".RESMED.PATIENTLIST_SRC LIMIT 5',
-};
-
-export default routes;
+export default {
+    patient: 'SELECT * from "LIVE DATA".RESMED.PATIENTLIST_SRC LIMIT 5',
+} as Record<string, string>;
