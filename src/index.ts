9a437f5a2425de9ceaea9d93aaabca4f99c61a0c
HM <hieumdd@gmail.com>
2022-06-17 23:39:20 +0700

    feat: Add connection middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import { http } from '@google-cloud/functions-framework';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 
-import patientController from './patient/patient.controller';
+import { connectionPromise } from './providers/snowflake';
+import patientController from './routes';
+
+const snowflake = async (req: Request, res: Response, next: NextFunction) => {
+    req.snowflake = await connectionPromise;
+    next();
+};
 
 const app = express();
+
+app.use(snowflake);
 app.use(patientController);
 
 http('main', app);
