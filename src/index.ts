1b3a2de66fab5cbd028d86a84da4e60046ba1f3e
HM <hieumdd@gmail.com>
2022-06-18 00:14:33 +0700

    refactor: Refactor into routes with params

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { http } from '@google-cloud/functions-framework';
 import express, { NextFunction, Request, Response } from 'express';
 
-import { connectionPromise } from './providers/snowflake';
-import patientController from './routes';
+import { execute, connectionPromise } from './providers/snowflake';
+import routes from './routes';
 
 const snowflake = async (req: Request, res: Response, next: NextFunction) => {
     req.snowflake = await connectionPromise;
@@ -12,6 +12,17 @@ const snowflake = async (req: Request, res: Response, next: NextFunction) => {
 const app = express();
 
 app.use(snowflake);
-app.use(patientController);
+
+app.get('/:route', (req: Request, res: Response) => {
+    const route = req.params.route;
+
+    if (!(route in routes)) {
+        res.status(404).json({ error: `Route ${route} not found` });
+        return;
+    }
+    execute(req.snowflake, routes[route])
+        .then((data) => res.json({ data }))
+        .catch((err) => res.status(500).json({ error: err.message }));
+});
 
 http('main', app);
