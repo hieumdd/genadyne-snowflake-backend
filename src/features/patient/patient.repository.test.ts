9f2958a82e826585057bb1dcfdb3ae5d31703bef
HM <hieumdd@gmail.com>
2022-06-23 21:15:02 +0700

    feat: Add PatientRepositoryTest

--- src/features/patient/patient.repository.test.ts
+++ src/features/patient/patient.repository.test.ts
@@ -1,26 +1,18 @@
-import { connectionPromise, disconnect } from '../../providers/snowflake';
 import PatientRepository, { Options } from './patient.repository';
 
 const page = { count: 10, page: 0 };
 const time = { start: '2020-01-01', end: '2023-01-01' };
 const patientName = { patientName: 'LAIRD  NANCY' };
 
-export const testCases: [string, Options][] = [
-    ['Query', { ...page }],
-    ['Query With Date', { ...page, ...time }],
-    ['Query with Patient Name', { ...page, ...patientName }],
-    ['Query with All', { ...page, ...time, ...patientName }],
+export const testCases: { name: string; options: Options }[] = [
+    { name: 'Query', options: { ...page } },
+    { name: 'Query With Date', options: { ...page, ...time } },
+    { name: 'Query with All', options: { ...page, ...time, ...patientName } },
+    { name: 'Query with Patient Name', options: { ...page, ...patientName } },
 ];
 
-describe('Patient Repository', () => {
-    it('Query', () => {
-        const query = PatientRepository(testCases[0][1]);
-        console.log(query);
-        
-    })
-    it.each(testCases)('', (_, options) => {
-        const query = PatientRepository(options);
-        console.log(query);
-        expect(query).toBeTruthy();
-    });
+it.each(testCases)('Query', ({ name, options }) => {
+    const query = PatientRepository(options).toQuery();
+    console.log({ name, query });
+    expect(query).toBeTruthy();
 });
