b3745fa9f63401007cc65f5d6348e24f7854e08b
HM <hieumdd@gmail.com>
2022-06-27 19:05:43 +0700

    feat: Add CORS

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,19 @@ app.use(async (req, res, next) => {
     next();
 });
 
+app.use((req, res, next) => {
+    res.set('Access-Control-Allow-Origin', '*');
+
+    if (req.method === 'OPTIONS') {
+        res.set('Access-Control-Allow-Methods', 'GET');
+        res.set('Access-Control-Allow-Headers', 'Content-Type');
+        res.set('Access-Control-Max-Age', '3600');
+        res.status(204).send('');
+    } else {
+        next();
+    }
+});
+
 app.get('/patient-session', PatientSessionController);
 app.get('/patient', PatientController);
 
