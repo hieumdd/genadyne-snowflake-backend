eb22f05b3193524a1a2de7627fea9bdd7e54bedb
HM <hieumdd@gmail.com>
2022-06-30 20:15:12 +0700

    fix: Fix typing

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,7 +1,7 @@
 import { Request } from 'express';
 import { Connection } from 'snowflake-sdk';
 
-import { Options } from '../features/pati../features/patient-session/patient.service
+import { Options } from '../features/patient-session/patient-session.service'
 
 declare module 'express-serve-static-core' {
     interface Request {
